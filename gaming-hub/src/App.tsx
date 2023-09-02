99bb034d1e0f9c7da7461ea8cc7df7ebb1b7f100
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-09-01 17:17:27 -0700

    Fetch the genres

--- gaming-hub/src/App.tsx
+++ gaming-hub/src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react"
 import Navbar from "./component/Navbar"
 import GameGrid from "./component/GameGrid"
+import GenreList from "./component/GenreList"
 
 
 function App() {
@@ -15,7 +16,9 @@ function App() {
           <Navbar />
         </GridItem>
         <Show above='lg'>
-        <GridItem area='aside' >Aside</GridItem>
+        <GridItem area='aside' >
+          <GenreList />
+        </GridItem>
         </Show>
         <GridItem area='main' >
          <GameGrid />  
---
+++ gaming-hub/src/hooks/useGenres.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import apiClient from "../Services/api-client";
+import { CanceledError } from "axios";
+interface Genre {
+    id: number;
+    name: string;
+}
+interface FetchGenresResponse {
+   count: number;
+   result: Genre[];
+}
+const useGenres = () => {
+    const [genres,setGenres] = useState<Genre[]>([]);
+    const [error, setError ] = useState('');
+    const [isLoading, setLoading] = useState(false);
+
+    useEffect(() => {
+      const controller = new AbortController();
+      setLoading(true)
+      apiClient
+      .get<FetchGenresResponse>('/genres',{signal: controller.signal})
+     .then(res => {
+       setGenres(res.data.results);
+       setLoading(false);
+    })
+     .catch(err => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+        setLoading(false);
+    });
+
+     return () => controller.abort();
+  }, [])
+
+  return { genres ,error, isLoading };
+}
+
+export default useGenres ;
