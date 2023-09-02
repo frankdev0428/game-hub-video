91766cd4a80432d6ac1a03fa1c42f28e3fb2d6e7
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-09-01 17:06:43 -0700

    Refactor remove dumlicated style

--- gaming-hub/src/component/GameGrid.tsx
+++ gaming-hub/src/component/GameGrid.tsx
@@ -3,6 +3,7 @@ import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
   const {games ,error , isLoading} = useGames();
@@ -12,10 +13,13 @@ const GameGrid = () => {
     <>
         {error && <Text>{error}</Text>}
         <SimpleGrid columns={ {sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding={10}>
-        {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
-        {games.map(game => 
+        {isLoading && skeletons.map(skeleton =><GameCardContainer>
+          <GameCardSkeleton key={skeleton} />
+        </GameCardContainer>)}
+        {games.map(game => <GameCardContainer>
           <GameCard key={game.id} game={game}/>
-          )}
+        </GameCardContainer>
+         )}
         </SimpleGrid>
     </>
   )
