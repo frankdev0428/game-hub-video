52bc822d2441a1d58fb4f559e0d7d5d5f0280117
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-09-07 21:20:01 -0700

    Handle game with no image

--- gaming-hub/src/Services/image-url.ts
+++ gaming-hub/src/Services/image-url.ts
@@ -1,8 +1,8 @@
-
+import noImage from '../Resources/asset/no-image.webp'
 
 
 const getCroppedImageUrl = (url : string) => {
-   if (!url) return  '';
+   if (!url) return  noImage;
    const target = 'media/';
    const index =  url.indexOf(target) + target.length;
    return  url.slice(0, index) + 'crop/600/400/' + url.slice(index);
